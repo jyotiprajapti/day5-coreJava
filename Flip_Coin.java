bcb65a9812f0bf8f6c0d5017eae1786ea6c330a3
jyotiprajapti <jyotiprajapati3029@gmail.com>
2023-01-19 22:46:20 +0530

    flipCoin added

---
+++ Flip_Coin.java
@@ -0,0 +1,30 @@
+package day5;
+import java.util.Scanner;
+public class Flip_Coin {
+	public static void main(String[] args) 
+	{
+		
+		double countHead=0;
+		double countTail=0;
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enetr number of tmes coin has to be flipped");
+		int n = sc.nextInt();
+		for(int i=1; i<=n;i++) {
+			
+		
+		if (Math.random() < 0.5){
+            System.out.println("Heads");
+           countHead=countHead+1;
+    }else{
+            System.out.println("Tails");
+            countTail=countTail+1;
+    }
+	}
+		double	tailPercentage =(((countTail)/n)*100);
+        double	headPercentage =(((countHead)/n)*100);
+		System.out.println("Precentage of heads is " + headPercentage );
+		System.out.println("Precentage of tail is " + tailPercentage );
+	}
+
+}
