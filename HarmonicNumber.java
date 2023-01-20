d6fdd45a6851bdfdf07df37bc7f93e9801a9aca1
jyotiprajapti <jyotiprajapati3029@gmail.com>
2023-01-20 20:23:40 +0530

    harmonic numers

---
+++ HarmonicNumber.java
@@ -0,0 +1,29 @@
+package day5;
+import java.util.Scanner;
+public class HarmonicNumber {
+	static double harmonic(int N)
+	{
+	    
+	    float value = 1;
+	 
+	    
+	    for (int i = 2; i <= N; i++) {
+	        value += (float)1 / i;
+	    }
+	 
+	    return value;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter value of N");
+		int N = sc.nextInt();
+		if(N==0) {
+			System.out.println("enter a ositive integer value");
+		}
+		
+		System.out.println(harmonic(N));
+	
+	
+
+}
+}
