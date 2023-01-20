e54c6de23c4325c9667c349f7cf992551ff6b07f
jyotiprajapti <jyotiprajapati3029@gmail.com>
2023-01-20 20:36:49 +0530

    swap numbers

---
+++ Swap2Numbers.java
@@ -0,0 +1,17 @@
+package day5;
+import java.util.Scanner;
+public class Swap2Numbers {
+	public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter X and Y :" );
+        int x =sc.nextInt();
+        int y = sc.nextInt();
+
+        int temp = x;
+        x=y;
+        y=temp;
+        System.out.println("Swap Two Number: "+x+" "+y);
+
+	}
+
+}
