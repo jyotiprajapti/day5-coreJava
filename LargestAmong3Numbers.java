96599f3bb7d1b449feb9f800aa9dfdf277a465b8
jyotiprajapti <jyotiprajapati3029@gmail.com>
2023-01-20 20:45:48 +0530

    largest among 3

---
+++ LargestAmong3Numbers.java
@@ -0,0 +1,22 @@
+package day5;
+
+public class LargestAmong3Numbers {
+	public static void main(String args[]){
+
+        int x= 77;
+        int y= 65;
+        int z= 66;
+
+
+        if(x>=y && x>=z){
+            System.out.println(x+" is The Largest Number");
+        }
+        else if(y>=z){
+            System.out.println(y+ " is The Largest Number");
+        }
+        else{
+            System.out.println(z+ " is The Largest Number");
+        }
+    }
+
+}
